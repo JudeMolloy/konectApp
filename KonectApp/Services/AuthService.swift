25d8c0035c846cbcafc270b3a29af52fd968e967
Jude Molloy <JudeMolloy07@hotmail.com>
2018-01-12 23:17:10 +0000

    Added create a user and sign in as well as added to the UI

---
+++ KonectApp/Services/AuthService.swift
@@ -0,0 +1,43 @@
+//
+//  AuthService.swift
+//  KonectApp
+//
+//  Created by Jude Molloy on 12/01/2018.
+//  Copyright © 2018 Jude Molloy. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["email": user.email, ]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+            
+        }
+    }
+    
+    func logInUser(withEmail email: String, andPassword password: String, logInComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                logInComplete(false, error)
+                return
+            }
+            
+            logInComplete(true, nil)
+            
+        }
+        
+    }
+}
